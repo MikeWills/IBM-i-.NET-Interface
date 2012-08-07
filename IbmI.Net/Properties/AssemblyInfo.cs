28ba8322d1ff56397c155b2ebb60263f204100ac
Mike Wills <mike@mikewills.me>
2012-08-07 12:14:54 -0500

    Added information so package is listed in NuGet

--- IbmI.Net/Properties/AssemblyInfo.cs
+++ IbmI.Net/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IbmI.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of classes for working with IBM i data in C# .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mike Wills")]
 [assembly: AssemblyProduct("IbmI.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ IBM i .NET Interface Classes
 This is a product of things I need in my daily programming when working with data on the IBM i in .NET. 
 
 All documentation will evetually make it's way to [the wiki](https://github.com/MikeWills/IBM-i-.NET-Interface/wiki).
+
+Now available in NuGet via `Install-Package IbmI.Net`
